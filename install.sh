afa483c3171235633433d93b92f748e8e5aa7e77
Javier Nogales <javier.nog76@gmail.com>
2020-08-04 16:07:58 +0200

    reorganize files

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "**************************************************"
+echo "* Wellcome to SLIMBOOK PROX grub theme installer *"
+echo "*                             by @_javiernogales *"
+echo "**************************************************"
+echo ""
+echo "You going to install grub theme. Do you want to continue? (Y/n)"
+read continue_option
+echo "your option is $continue_option"
