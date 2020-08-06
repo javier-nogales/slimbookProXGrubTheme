855499278ec8a2ac5c1b94006383b584f09e3494
Javier Nogales <javier.nog76@gmail.com>
2020-08-06 17:46:59 +0200

    installer.sh working

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 INSTALL_MSG="You going to install grub theme. Do you want to CONTINUE? (Y/n)"
+BASE_PATH=/home/jnogales/develop/temp
+THEME_PATH=$BASE_PATH/slimbook_prox_theme
 
 # set -x			# activate debugging from here
 
+###############################################################################
+# INFO
+###############################################################################
 echo "**************************************************"
 echo "* Wellcome to SLIMBOOK PROX grub theme installer *"
 echo "*                             by @_javiernogales *"
 echo "**************************************************"
 echo ""
-
+###############################################################################
+# INSTALLATION CONTINUE QUESTION
+###############################################################################
 count=0
 while [ $count -lt 3 ]
 do 
@@ -33,8 +40,21 @@ then
     exit 0
 fi
 
-echo "Installation CONTINUE"
+# theme folder
+echo "creating theme folder..."
+sudo mkdir $THEME_PATH
 
-#ROOT_PATH="/develop/projects/slimbookProXTheme"
+echo "loading fonts..."
+sudo cp ./src/fonts/* $THEME_PATH
+
+echo "loadin OS icons..."
+sudo cp -r ./src/icons $THEME_PATH
+
+echo "loading images..."
+sudo cp -r ./src/images $THEME_PATH
+sudo cp -r ./src/pixmap $THEME_PATH
+
+echo "copying theme.txt..."
+sudo cp ./src/theme.txt $THEME_PATH/theme.txt
 
 # set +x			# stop debugging from here
